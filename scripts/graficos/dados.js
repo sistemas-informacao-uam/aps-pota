3deccfedfa682184cda6fa78a18baea51b756533
paulohdelia <paulohdelia@gmail.com>
2020-11-16 21:29:06 -0300

    Adc parametro na url para usar vetor com 10.000 itens

--- scripts/graficos/dados.js
+++ scripts/graficos/dados.js
@@ -7,6 +7,10 @@ import quickSort from '../algoritmos/QuickSort.js';
 import radixSort from '../algoritmos/RadixSort.js';
 import selectionSort from '../algoritmos/SelectionSort.js';
 
+const urlParams = new URLSearchParams(window.location.search);
+const desejaFritarSeuPC =
+  urlParams.get('desejaFritarSeuPC')?.toLowerCase() === 'true';
+
 // 10000 gasta muito tempo para processar
 const esqueletoVetores = {
   5: [],
@@ -14,8 +18,28 @@ const esqueletoVetores = {
   50: [],
   100: [],
   1000: [],
-  // 10000: [],
 };
+
+if (desejaFritarSeuPC) {
+  esqueletoVetores[10000] = [];
+}
+
+const container = document.querySelector('.container');
+
+for (let i in esqueletoVetores) {
+  const chartsContainer = document.createElement('div');
+  const canvas = document.createElement('canvas');
+
+  chartsContainer.appendChild(canvas);
+
+  chartsContainer.classList.add('charts-container');
+
+  canvas.classList.add('chart');
+  canvas.style.width = '400px';
+  canvas.style.height = '400px';
+
+  container.appendChild(chartsContainer);
+}
 
 const vetores = gerarVetores(50, esqueletoVetores);
 
@@ -74,8 +98,8 @@ function print(sort, medias) {
     'color: yellow',
     'color: cyan',
     'color: yellow',
-    // 'color: cyan',
-    // 'color: yellow',
+    desejaFritarSeuPC ? 'color: cyan' : '',
+    desejaFritarSeuPC ? 'color: yellow' : '',
   );
 }
 
@@ -115,65 +139,18 @@ function obterMediaDeTrocas(vetores, algoritmo) {
   );
 }
 
-const data5 = [
-  mediasBubbleSort[5],
-  mediasHeapSort[5],
-  mediasMergeSort[5],
-  mediasCountSort[5],
-  mediasRadixSort[5],
-  mediasSelectionSort[5],
-];
-
-const data10 = [
-  mediasBubbleSort[10],
-  mediasHeapSort[10],
-  mediasMergeSort[10],
-  mediasCountSort[10],
-  mediasRadixSort[10],
-  mediasSelectionSort[10],
-];
-
-const data50 = [
-  mediasBubbleSort[50],
-  mediasHeapSort[50],
-  mediasMergeSort[50],
-  mediasCountSort[50],
-  mediasRadixSort[50],
-  mediasSelectionSort[50],
-];
-
-const data100 = [
-  mediasBubbleSort[100],
-  mediasHeapSort[100],
-  mediasMergeSort[100],
-  mediasCountSort[100],
-  mediasRadixSort[100],
-  mediasSelectionSort[100],
-];
-
-const data1000 = [
-  mediasBubbleSort[1000],
-  mediasHeapSort[1000],
-  mediasMergeSort[1000],
-  mediasCountSort[1000],
-  mediasRadixSort[1000],
-  mediasSelectionSort[1000],
-];
-
-// const data10000 = [
-//   mediasBubbleSort[10000],
-//   mediasHeapSort[10000],
-//   mediasMergeSort[10000],
-//   mediasSelectionSort[10000],
-// ];
-
-const dataset = [
-  { tamanho: 5, medias: data5 },
-  { tamanho: 10, medias: data10 },
-  { tamanho: 50, medias: data50 },
-  { tamanho: 100, medias: data100 },
-  { tamanho: 1000, medias: data1000 },
-  // { tamanho: 10000, medias: data10000 },
-];
+const dataset = Object.keys(esqueletoVetores).map(key => {
+  return {
+    tamanho: key,
+    medias: [
+      mediasBubbleSort[key],
+      mediasHeapSort[key],
+      mediasMergeSort[key],
+      mediasCountSort[key],
+      mediasRadixSort[key],
+      mediasSelectionSort[key],
+    ],
+  };
+});
 
 export { dataset };
